6716d5048735e8a6ef7a809f260e5701eb9f86e7
Pedro Carvalho <pedro.carvalho@emplifi.io>
2023-11-23 11:19:16 +0100

    chore: format code

--- cmd/ccwc/cli.js
+++ cmd/ccwc/cli.js
@@ -1,76 +1,80 @@
 #!/usr/bin/env node
 import { countBytes, countCharacters, countLines, countWords } from './helperFunctions.js'
 import fs from 'fs'
+
+
 const args = process.argv.slice(2);
+const log = console.log
+const errorMessage = "Not a valid command! Usage: ccwc -c|-l|-w|-m [fileName]"
 
 
-if (args.length === 0 ) {
-  console.log('Usage: ccwc -c|-l|-w|-m [fileName]');
+if (args.length === 0) {
+  log('Usage: ccwc -c|-l|-w|-m [fileName]');
   process.exit(0);
 }
 
+
 if (args.length > 2) {
-  console.error('Not a valid command! Usage: ccwc -c|-l|-w|-m [fileName]');
+  console.error(errorMessage);
   process.exit(1);
 }
 
+
 let command = args[0];
-
 let fileName = args[1];
-
 const commandOptions = ['-c', '-l', '-w', '-m']
 
 try {
   if (command === commandOptions[0]) {
-    if(!fileName) {
+    if (!fileName) {
       const fileContent = fs.readFileSync(0, 'utf-8');
-      console.log(`${countBytes(fileContent)}`)
+      log(`${countBytes(fileContent)}`)
       process.exit(0);
     }
     const fileContent = fs.readFileSync(fileName, 'utf-8');
-    console.log(`${countBytes(fileContent)} ${fileName}`);
+    log(`${countBytes(fileContent)} ${fileName}`);
     process.exit(0);
   }
   if (command === commandOptions[1]) {
-    if(!fileName) {
+    if (!fileName) {
       const fileContent = fs.readFileSync(0, 'utf-8');
-      console.log(`${countLines(fileContent)}`)
+      log(`${countLines(fileContent)}`)
       process.exit(0);
     }
     const fileContent = fs.readFileSync(fileName, 'utf-8');
-    console.log(`${countLines(fileContent)} ${fileName}`);
+    log(`${countLines(fileContent)} ${fileName}`);
     process.exit(0);
   }
   if (command === commandOptions[2]) {
-    if(!fileName) {
+    if (!fileName) {
       const fileContent = fs.readFileSync(0, 'utf-8');
-      console.log(`${countWords(fileContent)}`)
+      log(`${countWords(fileContent)}`)
       process.exit(0);
     }
     const fileContent = fs.readFileSync(fileName, 'utf-8');
-    console.log(`${countWords(fileContent)} ${fileName}`);
+    log(`${countWords(fileContent)} ${fileName}`);
     process.exit(0);
   }
   if (command === commandOptions[3]) {
-    if(!fileName) {
+    if (!fileName) {
       const fileContent = fs.readFileSync(0, 'utf-8');
-      console.log(`${countCharacters(fileContent)}`)
+      log(`${countCharacters(fileContent)}`)
       process.exit(0);
     }
     const fileContent = fs.readFileSync(fileName, 'utf-8');
-    console.log(`${countCharacters(fileContent)} ${fileName}`);
+    log(`${countCharacters(fileContent)} ${fileName}`);
     process.exit(0);
   }
   if (!command.includes(commandOptions)) {
     fileName = args[0];
     if (args.length > 1) {
-      console.error("Not a valid command! Usage: ccwc -c|-l|-w|-m [fileName]");
+      console.error(errorMessage);
       process.exit(1);
     }
     const fileContent = fs.readFileSync(fileName, 'utf-8');
-    console.log(`${countLines(fileContent)} ${countWords(fileContent)} ${countBytes(fileContent)} ${fileName}`)
+    log(`${countLines(fileContent)} ${countWords(fileContent)} ${countBytes(fileContent)} ${fileName}`)
   }
 } catch (err) {
-  console.error("Not a valid command! Usage: ccwc -c|-l|-w|-m [fileName]");
+  console.error(errorMessage);
   process.exit(1);
 }
